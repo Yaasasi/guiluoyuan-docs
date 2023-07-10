91ee521010990975666046bc7a5142f4914bba98
Yaasasi <yuxi@yaasasi.cn>
2023-07-10 10:44:04 +0800

    update to version 0.1.2

--- src/components/logo.js
+++ src/components/logo.js
@@ -4,9 +4,9 @@ import styles from './logo.module.css'
 export default function Logo() {
   return (
     <>
-      <Image src="/icons/64.png" alt="归落原" width="36" height="36" />
+      <Image src="/icons/GuiLuoYuan-Loge.png" alt="归落原" width="110" height="55" />
       <div className={styles.logo}>
-        <span className={styles.logo__title}>归落原 · 文档</span>
+        <span className={styles.logo__title}></span>
       </div>
     </>
   )
