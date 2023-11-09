b9d6120aa43d8dc21bd463e2de122945590b291b
Yaasasi <yuxi@yaasasi.cn>
2023-11-10 03:20:05 +0800

    update to version 0.3.1

--- src/theme.config.js
+++ src/theme.config.js
@@ -17,9 +17,9 @@ export default {
       <meta name="msapplication-TileColor" content="#ffffff" />
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="文档 · 归落原" />
-      <meta name="apple-mobile-web-app-title" content="文档 · 归落原" />
+      <meta httpEquiv="Content-Language" content="zh" />
+      <meta name="description" content="Ourmoe" />
+      <meta name="apple-mobile-web-app-title" content="Ourmoe" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
